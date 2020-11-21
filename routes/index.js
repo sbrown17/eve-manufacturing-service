be60ef2c322ed65746fdbd23c49007e68e206b4f
Stuart Brown <stubrown357@gmail.com>
2020-11-21 02:20:13 +0000

    include constants for esiJS

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const esiJS = require('esiJS')
+const esiClient = new esiJS()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
