f04f5530c1a10ef0f8d244b2aaab69a4cd7dea9d
sbrown17 <stubrown357@gmail.com>
2020-11-24 20:57:14 -0500

    remove esiJS related code for now

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const esiJS = require('esiJS')
-const esiClient = new esiJS()
+//const esiJS = require('esiJS')
+//const esiClient = new esiJS()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
